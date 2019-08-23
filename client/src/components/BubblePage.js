0456f68ce0788c3ef77ab0e255f052f97e0c1734
tjvonbr <51134664+tjvonbr@users.noreply.github.com>
2019-08-23 13:15:14 -0500

    Completed MVP--ugly, but complete

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -1,17 +1,53 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
 
 import Bubbles from "./Bubbles";
 import ColorList from "./ColorList";
+import axiosWithAuth from "../utils/axiosWithAuth";
 
 const BubblePage = () => {
   const [colorList, setColorList] = useState([]);
-  // fetch your colors data from the server when the component mounts
-  // set that data to the colorList state property
+
+  useEffect(() => {
+    axiosWithAuth()
+      .get('http://localhost:5000/api/colors')
+      .then(response => {
+        setColorList(response.data)
+      })
+      .catch(error => console.log(error.response))
+  }, [])
+
+  const saveEdit = color => {
+    axiosWithAuth()
+      .put(`http://localhost:5000/api/colors/${color.id}`, color)
+      .then(response => {
+        console.log(response)
+        let newColorList = colorList.map(color => {
+          if (color.id === response.data.id) {
+            return response.data
+          } else {
+            return color
+          }
+        })
+        setColorList(newColorList);
+      })
+      .catch(error => console.log(error.response))
+  };
+
+  const deleteColor = deleteColor => {
+    axiosWithAuth()
+      .delete(`http://localhost:5000/api/colors/${deleteColor.id}`)
+      .then(response => {
+        console.log(response.data)
+      })
+      .catch(error => console.log(error.response));
+  };
 
   return (
     <>
-      <ColorList colors={colorList} updateColors={setColorList} />
+      <ColorList colors={colorList}
+                  updateColors={setColorList} 
+                  saveEdit={saveEdit} 
+                  deleteColor={deleteColor} />
       <Bubbles colors={colorList} />
     </>
   );
--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,12 +1,12 @@
 import React, { useState } from "react";
-import axios from "axios";
+import axiosWithAuth from "../utils/axiosWithAuth";
 
 const initialColor = {
   color: "",
   code: { hex: "" }
 };
 
-const ColorList = ({ colors, updateColors }) => {
+const ColorList = ({ colors, updateColors, saveEdit, removeColor }) => {
   console.log(colors);
   const [editing, setEditing] = useState(false);
   const [colorToEdit, setColorToEdit] = useState(initialColor);
@@ -16,15 +16,13 @@ const ColorList = ({ colors, updateColors }) => {
     setColorToEdit(color);
   };
 
-  const saveEdit = e => {
+  const saveColor = e => {
     e.preventDefault();
-    // Make a put request to save your updated color
-    // think about where will you get the id from...
-    // where is is saved right now?
+    saveEdit(colorToEdit);
   };
 
   const deleteColor = color => {
-    // make a delete request to delete this color
+    removeColor(color);
   };
 
   return (
@@ -47,7 +45,7 @@ const ColorList = ({ colors, updateColors }) => {
         ))}
       </ul>
       {editing && (
-        <form onSubmit={saveEdit}>
+        <form onSubmit={saveColor}>
           <legend>edit color</legend>
           <label>
             color name:
